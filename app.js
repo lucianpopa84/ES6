5adb05127d0d322c592b9602178f37b26691e640
lucianpopa84 <lucianpopa84@gmail.com>
2019-01-18 22:03:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,31 @@ $(document).ready(function () {
     myRequest.open('GET', 'https://lucianpopa84.github.io/ES6/students.json');
     myRequest.onload = function () {
       var studentsData = JSON.parse(myRequest.responseText);
+      renderStudentsHTML(studentsData);
+    };
+    myRequest.send();
+  });
 
-      if ($(".card")[0]) {
-        $(".card").remove(); // remove previous cards
-      }
+  $('#teachersLoad').click(function () {
+    var myRequest = new XMLHttpRequest();
+    myRequest.open('GET', 'https://lucianpopa84.github.io/ES6/teachers.json');
+    myRequest.onload = function () {
+      var teachersData = JSON.parse(myRequest.responseText);
+      renderTeachersHTML(teachersData);
+    };
+    myRequest.send();
+  });
 
-      var htmlContent = `<div class="card-deck">`;
-      for (k = 0; k < studentsData.length; k++) {
-        var { firstName, lastName, averageGrade } = studentsData[k];
-        htmlContent += `
+  function renderStudentsHTML(data) {
+
+    if ($(".card")[0]) {
+      $(".card").remove(); // remove previous cards
+    }
+
+    var htmlContent = `<div class="card-deck">`;
+    for (k = 0; k < data.length; k++) {
+      var { firstName, lastName, averageGrade } = data[k];
+      htmlContent += `
             <div class="card bg-light mb-3 border-secondary">
             <img class="card-img-top" src="https://lucianpopa84.github.io/ES6/Media/s${k + 1}.jpg" alt="Card image">
             <div class="card-body">
@@ -24,47 +40,37 @@ $(document).ready(function () {
           </div>
           </div>
           `;
-      }
-      htmlContent += `</div>`;
+    }
+    htmlContent += `</div>`;
 
-      $(".card-deck").replaceWith(htmlContent);
-    };
+    $(".card-deck").replaceWith(htmlContent);
+  }
 
-    myRequest.send();
-  });
+  function renderTeachersHTML(data) {
 
-  $('#teachersLoad').click(function () {
-    var myRequest = new XMLHttpRequest();
-    myRequest.open('GET', 'https://lucianpopa84.github.io/ES6/teachers.json');
-    myRequest.onload = function () {
-      var teachersData = JSON.parse(myRequest.responseText);
+    if ($(".card")[0]) {
+      $(".card").remove(); // remove previous cards
+    }
 
-      if ($(".card")[0]) {
-        $(".card").remove(); // remove previous cards
-      }
-
-      var htmlContent = `<div class="card-deck">`;
-      for (k = 0; k < teachersData.length; k++) {
-        var { firstName, lastName, title } = teachersData[k];
-        htmlContent += `
-            <div class="card bg-light mb-3 border-secondary">
-            <img class="card-img-top" src="https://lucianpopa84.github.io/ES6/Media/t${k + 1}.jpg" alt="Card image">
-            <div class="card-body">
-              <h5 class="card-title">${firstName} ${lastName}</h5>
-              <p class="card-text">Teacher</p>
-            </div>
-            <div class="card-footer">
-            Title: ${title}
+    var htmlContent = `<div class="card-deck">`;
+    for (k = 0; k < data.length; k++) {
+      var { firstName, lastName, title } = data[k];
+      htmlContent += `
+          <div class="card bg-light mb-3 border-secondary">
+          <img class="card-img-top" src="https://lucianpopa84.github.io/ES6/Media/t${k + 1}.jpg" alt="Card image">
+          <div class="card-body">
+            <h5 class="card-title">${firstName} ${lastName}</h5>
+            <p class="card-text">Teacher</p>
           </div>
-          </div>
-          `;
-      }
-      htmlContent += `</div>`;
+          <div class="card-footer">
+          Title: ${title}
+        </div>
+        </div>
+        `;
+    }
+    htmlContent += `</div>`;
 
-      $(".card-deck").replaceWith(htmlContent);
-    };
-
-    myRequest.send();
-  });
+    $(".card-deck").replaceWith(htmlContent);
+  }
 
 });
